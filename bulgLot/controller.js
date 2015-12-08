6ec363cb4f02289cd214f39bd0077d09f2e3c879
JanKukovic <kukovicster@gmail.com>
2015-12-08 15:45:46 +0100

    Domaca naloga

---
+++ bulgLot/controller.js
@@ -0,0 +1,69 @@
+angular.module('app', []);
+
+
+
+angular.module('app')
+  .controller('DemoController', function($scope, $q){
+
+
+  
+
+
+  	function obljubaSt(){
+  		
+  		var deferred = $q.defer();
+  		var timeout = nakljucnostevilo(2000,5000);
+  		var temp= [];
+  		 temp.st = preveriPodvajanje(nakljucnostevilo(1,100));
+  		 temp.delay = timeout;
+
+  		 	
+
+  		 setTimeout(function() {
+
+  		 	deferred.resolve(temp);
+       		} , timeout);
+
+
+        return deferred.promise;
+
+  	};
+
+  	$scope.zrebaj = function(){
+  		$scope.zrebanja = [];
+
+  		obljubaSt().then(function(result) { 
+  		$scope.zrebanja.push(result);
+  			obljubaSt().then(function(result) { 
+	  		$scope.zrebanja.push(result);
+	  			obljubaSt().then(function(result) { 
+		  		$scope.zrebanja.push(result);
+		  		}, function(error) {
+			        alert("napaka");
+			    });
+	  		}, function(error) {
+		        alert("napaka");
+		    });
+  		}, function(error) {
+	        alert("napaka");
+	    });
+  	};
+
+  	var preveriPodvajanje = function(st){
+  		
+  		$scope.zrebanja.forEach(function(zreb){
+  		 	if(st == zreb.st){
+  			 st = nakljucnostevilo(1,100);
+  			 st = preveriPodvajanje(st);
+  		 	};
+  		 });
+
+  		return st
+  	};
+
+  	function nakljucnostevilo(min,max){
+  		return Math.floor(Math.random()*(max-min+1)+min);
+  	};
+
+
+  });
